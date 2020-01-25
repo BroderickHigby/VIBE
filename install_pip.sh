e43e4578f636fd6ff87c7ef9349d66aabe5110e1
Broderick Higby <broderick.higby@gmail.com>
2020-01-24 21:57:11 -0800

    install cuda and cudnn

--- install_pip.sh
+++ install_pip.sh
@@ -1,5 +1,43 @@
 #!/usr/bin/env bash
 
+# install CUDA 10
+echo "Checking for CUDA and installing."
+echo "---------------------------------"
+# Check for CUDA and try to install.
+if [[ "$(dpkg-query -W cuda)" != *"10."* ]]; then
+  sudo apt-get install dirmngr -y
+  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub  | sudo apt-key add -
+  curl -O http://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda-repo-ubuntu1604-10-1-local-10.1.243-418.87.00_1.0-1_amd64.deb
+  sudo dpkg -i ./cuda-repo-ubuntu1604-10-1-local-10.1.243-418.87.00_1.0-1_amd64.deb
+  sudo apt-key add /var/cuda-repo-10-1-local-10.1.243-418.87.00/7fa2af80.pub
+  sudo apt-get update
+  sudo apt-get install cuda -y
+fi
+
+if [ -z "$(cat /usr/local/cuda/include/cudnn.h | grep CUDNN_MAJOR -A 2)" ]; then
+  # Install cuDNN 5.1
+  echo "Install cuDNN 5.1"
+  echo "-----------------"
+  CUDNN_URL="http://developer.download.nvidia.com/compute/redist/cudnn/v5.1/cudnn-8.0-linux-x64-v5.1.tgz"
+  wget -c ${CUDNN_URL}
+  sudo tar -xzf cudnn-8.0-linux-x64-v5.1.tgz -C /usr/local
+  rm cudnn-8.0-linux-x64-v5.1.tgz && sudo ldconfig
+fi
+
+
+
+echo "Set CUDA env vars"
+echo "-----------------"
+
+echo 'export CUDA_HOME=/usr/local/cuda' >> ~/.bashrc
+
+echo 'export PATH=$PATH:$CUDA_HOME/bin' >> ~/.bashrc
+
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_HOME/lib64' >> ~/.bashrc
+
+source ~/.bashrc
+
+
 if [[ "$(python3.7 --version)" != *"3.7"* ]]; then
   echo "Install python 3.7"
   echo "------------------"
